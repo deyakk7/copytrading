5bc97eccaa88b9aceb4f2335b41218bcf3bbe0a1
deyakk <deiak.yevhenii@gmail.com>
2024-03-19 14:20:35 +0200

    add: deleting photos from folder

--- trader/models.py
+++ trader/models.py
@@ -1,4 +1,8 @@
+import os
+
 from django.db import models
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
 
 
 class Trader(models.Model):
@@ -9,3 +13,32 @@ class Trader(models.Model):
 
     def __str__(self):
         return f"{self.nickname}"
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_photo = Trader.objects.get(pk=self.pk).photo
+            new_photo = self.photo
+            if old_photo and old_photo != new_photo:
+                if os.path.isfile(old_photo.path):
+                    os.remove(old_photo.path)
+        super(Trader, self).save(*args, **kwargs)
+
+
+@receiver(post_delete, sender=Trader)
+def photo_post_delete_handler(sender, **kwargs):
+    instance = kwargs['instance']
+    if instance.photo:
+        if os.path.isfile(instance.photo.path):
+            os.remove(instance.photo.path)
+
+
+@receiver(pre_save, sender=Trader)
+def photo_pre_save_handler(sender, instance, **kwargs):
+    if instance.pk:
+        old_photo = Trader.objects.get(pk=instance.pk).photo
+        new_photo = instance.photo
+        if old_photo and old_photo != new_photo:
+            if os.path.isfile(old_photo.path):
+                os.remove(old_photo.path)
+
+
