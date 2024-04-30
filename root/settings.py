4e837612182eb6e3f169631f92d521ecd095007e
deyakk <deiak.yevhenii@gmail.com>
2024-04-30 17:14:19 +0300

    fix admin

--- root/settings.py
+++ root/settings.py
@@ -63,7 +63,10 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "https://finrextest.com",
+    "http://finrextest.com",
     "https://www.finrex.com",
 ]
 
