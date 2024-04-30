af6a85c7c3f1fe875d624d5014a0f2ac6f43c2a8
deyakk <deiak.yevhenii@gmail.com>
2024-04-30 13:44:59 +0300

    fix: ssl del

--- root/settings.py
+++ root/settings.py
@@ -64,8 +64,6 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://finrextest.com",
