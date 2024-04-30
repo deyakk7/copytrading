8f7a09f6d5c1284a3f459c4d6eb7cdf6a659f092
deyakk <deiak.yevhenii@gmail.com>
2024-04-30 13:45:52 +0300

    del: origin all

--- root/settings.py
+++ root/settings.py
@@ -61,9 +61,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://finrextest.com",
