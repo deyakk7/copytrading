dcae2a3903e833ce5401ba17700bcb3840f235ba
deyakk <deiak.yevhenii@gmail.com>
2024-04-30 11:38:47 +0300

    fix: admin trending info

--- trader/admin.py
+++ trader/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from trader.models import Trader, TraderProfitHistory
+from trader.models import Trader, TraderProfitHistory, TrendingThreshold
 
 admin.site.register(Trader)
 admin.site.register(TraderProfitHistory)
+admin.site.register(TrendingThreshold)
